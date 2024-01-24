18957bd8fc6766f69e77d6abbbc2f2863c18906b
Елена Бархатова <hell_sun.net@inbox.ru>
2024-01-24 19:19:27 +0300

    использованы шаблонные строки для создания задач

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,60 +1,40 @@
-//Находим все  нужные элементы на странице
+"use strict";
 let taskInput = document.getElementById('task__input');
 let tasksList = document.getElementById('tasks__list');
 let taskForm = document.getElementById('tasks__form');
+let addTaskButton = document.getElementById('tasks__add');
 
-//Функция создания элемента задачи
-let makeElement = function (text) {
-    let element1 = document.createElement('div');
-    element1.classList.add("task");
+// Функция добавления задачи с использованием шаблонных строк
+let addTask = function (text) {
+    let taskHTML = `
+        <div class="task">
+            <div class="task__title">
+                ${text}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>
+    `;
 
-    let element2 = document.createElement('div');
-    element2.classList.add("task__title");
-    element2.textContent = text;
-
-    let element3 = document.createElement('a');
-    element3.href = "#";
-    element3.classList.add("task__remove");
-    element3.innerHTML = "&times;";
-
-    element1.appendChild(element2);
-    element1.appendChild(element3);
-
-    element3.onclick = function(e) {
-        e.target.closest('.task').remove();
-    };
-
-    return element1;
+    tasksList.insertAdjacentHTML('afterbegin', taskHTML);
 };
 
-// Функция добавления задачи
-let addTask = function () {
-    if (taskInput.value.trim() !== '') {
-        let text = taskInput.value.trim();
-        tasksList.appendChild(makeElement(text));
-        taskInput.value = '';
-    }
-};
-
-// Обработчик события нажатия клавиши Enter
-taskForm.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        addTask();
-    }
+// Вешаем обработчик события на форму
+taskForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    addTask(taskInput.value.trim());
+    taskInput.value = '';
 });
 
-
-//Вешаем обработчик события на форму,  клавиша enter будет задействована по умолчанию
-// taskForm.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     addTask();
-// });
-
 // Обработчик события клика по кнопке "Добавить"
-// let addTaskButton = document.getElementById('tasks__add');
-// 
-// addTaskButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     addTask();
-// });
+addTaskButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    addTask(taskInput.value.trim());
+    taskInput.value = '';
+});
+
+// Обработчик события клика по кнопке удаления
+tasksList.addEventListener('click', function (e) {
+    if (e.target.classList.contains('task__remove')) {
+        e.target.closest('.task').remove();
+    }
+});
