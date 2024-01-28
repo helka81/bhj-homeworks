5d157c53eb72ccc941bc71963c243595c4ea5aa4
Елена Бархатова <hell_sun.net@inbox.ru>
2024-01-28 15:58:53 +0300

    прогрессбар сделан

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,41 @@
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.upload.addEventListener('progress', (event) => {
+    if (event.lengthComputable) {
+      const percentComplete = (event.loaded / event.total) * 100;
+      // Обновление значения элемента <progress>
+      progress.value = percentComplete.toFixed(1);
+        
+    }
+  });
+
+  xhr.addEventListener('load', function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      try {
+        // Обработка успешного ответа
+        console.log('Успешно:', xhr.responseText);
+      } catch (error) {
+        console.error('Ошибка при разборе JSON:', error);
+      }
+    } else {
+      console.error('Ошибка запроса: ' + xhr.status);
+    }
+  });
+
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+
+  // Создание объекта FormData и добавление данных из формы
+  const formData = new FormData(form);
+
+  xhr.addEventListener('error', function () {
+    console.error('Сетевая ошибка');
+  });
+
+  xhr.send(formData);
+});
